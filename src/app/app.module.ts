67f8a53f5f89ac775c418fa655cc3b652f501112
Your Name <usmaninobello@gmail.com>
2023-11-07 11:19:06 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { MyCounterComponent } from './my-counter/my-counter.component';
+import { count } from 'rxjs';
+import { counterReducer } from './counter.reducers';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyCounterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({ count: counterReducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
